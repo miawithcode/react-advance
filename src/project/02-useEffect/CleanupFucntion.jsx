6e21c7f5ae08f46c52974074202396511ddfe83b
miawithcode <cuttontail@gmail.com>
2023-12-31 04:18:40 +0800

    feat: add cleanup function for eventListener

--- src/project/02-useEffect/CleanupFucntion.jsx
+++ src/project/02-useEffect/CleanupFucntion.jsx
@@ -13,13 +13,21 @@ const CleanupFucntion = () => {
 
 const RandomComponent = () => {
   useEffect(() => {
-    console.log("Hello from useEffect");
-    const interveId = setInterval(() => {
-      console.log("Hello from interval");
-    }, 1000); // This will keep running even the component is not display anymore if cleanup function is not set
-    return () => {
-      clearInterval(interveId); // stop interval when component is not display
+    // ===== setInterval Example =====
+    // console.log("Hello from useEffect");
+    // const interveId = setInterval(() => {
+    //   console.log("Hello from interval");
+    // }, 1000);
+    // return () => {
+    //   clearInterval(interveId);
+    // }
+
+    // ===== event Listener Example =====
+    const someFunc = () => {
+      // some logic
     }
+    window.addEventListener('scroll', someFunc);
+    return () => window.removeEventListener('scroll', someFunc);
   }, []);
 
   return <h1>Hello There</h1>
