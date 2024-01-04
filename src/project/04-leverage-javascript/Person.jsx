97e389e245d5ce51436bfc6c1c1afd5c2e108c43
miawithcode <cuttontail@gmail.com>
2024-01-05 01:00:36 +0800

    feat: use and operator to render image if one is missing

--- src/project/04-leverage-javascript/Person.jsx
+++ src/project/04-leverage-javascript/Person.jsx
@@ -1,9 +1,10 @@
 export function Person({ name, nickName='Default', images }) {
-  // const img = images[0].small.url;
+
+  const img = images && images[0] && images[0].small && images[0].small.url;
 
   return (
     <div>
-      {/* <img src={img} alt={name} style={{ width: '150px' }} /> */}
+      <img src={img} alt={name} style={{ width: '50px' }} />
       <h4>{name}</h4>
       <p>Nickname : { nickName}</p>
     </div>
