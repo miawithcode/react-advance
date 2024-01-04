8d6f9bc23f83a3e974a0f84d469b5387cdcaf612
miawithcode <cuttontail@gmail.com>
2024-01-05 01:03:53 +0800

    feat: use optional chaining to render image if one is missing

--- src/project/04-leverage-javascript/Person.jsx
+++ src/project/04-leverage-javascript/Person.jsx
@@ -1,6 +1,8 @@
 export function Person({ name, nickName='Default', images }) {
 
-  const img = images && images[0] && images[0].small && images[0].small.url;
+  // const img = images && images[0] && images[0].small && images[0].small.url;
+
+  const img = images?.[0]?.small?.url;
 
   return (
     <div>
