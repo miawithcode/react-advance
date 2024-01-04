260f7c45c8c7d3183f69a8a018c99946064af943
miawithcode <cuttontail@gmail.com>
2024-01-05 01:10:56 +0800

    feat: add default image if one is missing

--- src/project/04-leverage-javascript/Person.jsx
+++ src/project/04-leverage-javascript/Person.jsx
@@ -1,14 +1,15 @@
-export function Person({ name, nickName='Default', images }) {
+import avatar from '../../assets/default-avatar.svg';
 
-  // const img = images && images[0] && images[0].small && images[0].small.url;
+export function Person({ name, nickName = 'Default', images }) {
 
-  const img = images?.[0]?.small?.url;
+  // const img = images?.[0]?.small?.url || avatar;
+  const img = images?.[0]?.small?.url ?? avatar;
 
   return (
     <div>
       <img src={img} alt={name} style={{ width: '50px' }} />
       <h4>{name}</h4>
-      <p>Nickname : { nickName}</p>
+      <p>Nickname : {nickName}</p>
     </div>
   );
 }
