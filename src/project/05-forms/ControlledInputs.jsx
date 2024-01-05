2841ac2dcd5d42447533519018154fee2c25c97f
miawithcode <cuttontail@gmail.com>
2024-01-05 16:08:10 +0800

    feat: add setEmail

--- src/project/05-forms/ControlledInputs.jsx
+++ src/project/05-forms/ControlledInputs.jsx
@@ -9,11 +9,21 @@ const ControlledInputs = () => {
       <h4>Controlled Inputs</h4>
       <div>
         <label htmlFor="name">name</label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="email">email</label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
