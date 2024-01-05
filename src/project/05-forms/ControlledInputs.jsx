3b3311ca3ecda64f757349d8525231065200baec
miawithcode <cuttontail@gmail.com>
2024-01-05 16:06:31 +0800

    refactor: use arrow function in onChange

--- src/project/05-forms/ControlledInputs.jsx
+++ src/project/05-forms/ControlledInputs.jsx
@@ -4,16 +4,12 @@ const ControlledInputs = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
-
   return (
     <form>
       <h4>Controlled Inputs</h4>
       <div>
         <label htmlFor="name">name</label>
-        <input type="text" id="name" value={name} onChange={handleChange} />
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
         <label htmlFor="email">email</label>
